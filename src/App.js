bda11915608410c59d5b65472ca923317c88be0c
MANI12121212 <101326938+MANI12121212@users.noreply.github.com>
2023-04-15 22:58:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import Home from './Component/Home';
 import Products from './Routing/Products';
 import ProductDetails from './Routing/ProductDetails';
 import Contact from './Routing/Contact';
+import Protect from './Routing/Protect';
+import Technologies from './Routing/Technologies';
+import Html from './Routing/Html';
+import Css from './Routing/Css';
+import Javascript from './Routing/Javascript';
+import Reacts from './Routing/Reacts';
+
 function App() {
   const {isloggedIn} = useContext(AuthContext)
   return (
@@ -16,9 +23,17 @@ function App() {
       {isloggedIn ? <Nav/> :null}
       <Routes>
         <Route path='/' element = {isloggedIn ? <Home/> : <Login/>}/> 
-         <Route path='/products' element = {<Products/>}/>
-       <Route path='/productdetails/:id' element = {<ProductDetails/>}/>
+         <Route path='/products' element = { <Protect><Products/></Protect>    }/>
+       <Route path='/productdetails/:id' element = {   <ProductDetails/>}/>
       <Route path='/contact' element={<Contact/>}/>
+      <Route path='/tech' element = {     <Protect><Technologies/></Protect>}>
+      <Route  path='html' element = {<Html/>} />
+      <Route  path='css' element = {<Css/>} />
+      <Route  path='js' element = {<Javascript/>} />
+      <Route  path='react' element = {<Reacts/>} />
+
+      </Route>
+      
       </Routes>
     
     </div>
